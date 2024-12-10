4ba5b6135680e46d2331254071a4b51906f7eaf1
Eli <E@F.C>
2024-12-10 08:38:21 -0800

    pee

--- Player.java
+++ Player.java
@@ -1,12 +1,18 @@
-import java.util.Scanner;
 
-public class Player
-{
+public class Player {
     // Attributes
-
+    private int score;
+    private String name;
+    private int pronouns; // 1. he/him 2. she/her 3. they/them
     // Constructers
 
+    public Player() {
+
+    }
     // Accessors
 
     // Mutators
+    public void addScore() {
+
+    }
 }
