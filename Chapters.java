7025561d7bccedf828d48fccc5e9a7ed655d3ab3
Eli <E@F.C>
2025-01-17 14:08:05 -0800

    Added Chapters Class + Small Adjustments

---
+++ Chapters.java
@@ -0,0 +1,10 @@
+public class Chapters
+{
+    // Attributes
+
+    // Constructers
+
+    // Accessors
+
+    // Mutators
+}
--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+        Scanner myObj = new Scanner(System.in)
+        System.out.println("Welcome To Hangman");
 
-}
+        }
+    }
