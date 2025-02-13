94a9dd5cde6734502c699c6472ee39c12b7d654c
Eli <E@F.C>
2025-02-13 09:56:42 -0800

    started adding half baked visual hangman effects. gonna blow my brains out. need to finish...

--- HangmanGame.java
+++ HangmanGame.java
@@ -5,21 +5,57 @@ public class HangmanGame
     Scanner myObj = new Scanner(System.in);
     // Attributes
     String word;
-    String wrongGuesses;
-    String correctGuesses;
-    String pickedLetters;
+    String modifiedword;
+    int wrongGuesses;
+    int correctGuesses;
+    String pickedLetters = "";
     String input;
+    String buildWord = "_";;
     // Constructers
     public HangmanGame(String customWord) {
         if (customWord.equals("")) { // Made for endless mode.
-        this.word = WordProvider.getWord();
+        this.word = WordProvider.getWord().toUpperCase();
         }
 
         else { // Made For Storymode
             System.out.println("(Keep in mind the word is pregenerated.)");
         }
-        System.out.println("Alright, Pick a letter from your keyboard to guess.");
-        input = myObj.nextLine();
+
+        for (int i = 0; i <= this.word.length()-1; i++) {
+            buildWord+=" _";
+        }
+
+        System.out.println(buildWord);
+        this.word = this.word.toUpperCase();
+        this.modifiedword = this.word;
+        System.out.println("Alright, Pick a letter from your keyboard to guess."); // Game Start
+        while (wrongGuesses <= 6) {
+            System.out.println(buildWord);
+            input = myObj.nextLine();
+            input = input.toUpperCase();
+            if (input.length() == 1) {
+                if (pickedLetters.indexOf(input) == -1) {
+                    pickedLetters += input;
+                    if (modifiedword.indexOf(input) != -1) {
+                        correctGuesses++;
+                        System.out.println("Correct Guess!");
+                        this.modifiedword = this.modifiedword.substring(0,this.modifiedword.indexOf(input)-1)+this.modifiedword.substring(this.modifiedword.indexOf(input)+1);
+                        System.out.println("Modified Word:"+this.modifiedword);
+                        System.out.println("Word: "+this.word);
+                    }
+                    else {
+                        wrongGuesses++;
+                        System.out.println("Wrong Guess!");
+                    }
+                }
+                else {
+                    System.out.println("You've already picked that letter.");
+                }
+            }
+            else {
+                System.out.println("One Letter Response Only Please.");
+            }
+        }
     }
 
     // Accessors
